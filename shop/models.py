13ff781e3741482bc9812676bc15dd63676b9372
kenken999 <kenken999@users.noreply.huggingface.co>
2024-06-13 12:06:47 +0000

    Initial commit

---
+++ shop/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Shop(models.Model):
+    name = models.CharField(max_length=255)
+    category = models.CharField(max_length=255)
+    subcategory = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return self.name
